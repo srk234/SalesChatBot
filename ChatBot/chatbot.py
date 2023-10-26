75498d001035c4cde55f23c78a368be11bbbab4e
Sreekutty.V.S <sree.2.1997@gmail.com>
2023-10-26 15:56:06 +0530

    chatbot

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,15 @@
+from chatterbot import ChatBot
+from chatterbot.conversation import Statement
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatBot = ChatBot('ChatBot')
+trainer = ChatterBotCorpusTrainer(chatBot)
+
+trainer.train("chatterbot.corpus.english")
+
+# print(chatBot.get_response("what is AI?"))
+
+print("Hi, I am ChatBot")
+while True:
+    query = input(">>>")
+    print(chatBot.get_response(Statement(text=query, search_text=query)))
